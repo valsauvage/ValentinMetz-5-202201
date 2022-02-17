157e4c72e5073b7099832642a29f8c31d435323b
valsauvage <valsauvage@valsauvage.com>
2022-02-17 22:46:18 +0100

    récupération du nom de l'onjet 0 depuis l'API

--- front/js/script.js
+++ front/js/script.js
@@ -1,30 +1,25 @@
-function getProducts() {
-    fetch("http://localhost:3000/api/products")
-        .then(function (res) {
-            if (res.ok) {
-                return res.json();
-            }
-        })
-        .then(function (value) {
-            console.log(value);
-        })
-        .catch(function (err) {
-            console.log('Erreur');
-        });
-};
+fetch("http://localhost:3000/api/products")
+    .then(function (res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function (value) {
+        console.log(value);
 
+        const productList = value.length;
 
-async function displayProducts() {
-    const itemsList = document.getElementById('items');
-    const produits = await getProducts();
-    const productsNumber = await getProducts.length;
+        // function getProducts() {
+        //     for (let i = 0; i < productList; i++) {
+        //         i = i++;
+        //     }
+        // };
 
-    for (let i = 0; i < productsNumber; i++) {
-        product += i;
-        
-        itemsList.insertAdjacentHTML('afterend', ' <a href=' + getProducts.url + '"><article><img src="' + getProducts.imageUrl + '.../product01.jpg" alt="' + getProducts.altTxt + '"><h3 class="productName">' + getProducts.name + '</h3><p class="productDescription">' + getProducts.description + '</p></article></a>');
-    }
-    
+        document
+            .getElementById('name')
+            .innerHTML = value[0].name;
 
-
-}
+    })
+    .catch(function (err) {
+        console.log('Erreur');
+    });
